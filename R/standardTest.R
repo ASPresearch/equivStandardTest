4c02bc7abfc7d50e5df4dd17619b56cf64ab74fd
alexsanchezpla <asanchez@ub.edu>
2020-04-13 13:58:39 +0200

    PAckage extension to include Restricted and Unrestricted crosstabulation

--- R/standardTest.R
+++ R/standardTest.R
@@ -39,7 +39,8 @@ enrichOnto <- function (geneList,
            pAdjustMethod = pAdjustMeth, pvalueCutoff = pvalCutoff, qvalueCutoff = qvalCutoff, readable=TRUE)
 }
 
-#' crossTabGOIDs
+
+#' crossTabGOIDsUnrestricted
 #'
 #' This function performs a crosstabulation between two lists of enriched GOTerms
 #' The lists are intended to have been obtained from enrichment analyses performed on two gene lists
@@ -56,10 +57,10 @@ enrichOnto <- function (geneList,
 #'  names4lists<- names(kidneyEnrichedGOIDs)[1:2]
 #'  anOnto <- 'BP'
 #'  GOLev<- 3
-#'  crossTabbedGOIds <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, 
+#'  crossTabbedGOIds <- crossTabGOIDsUnrestricted (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, 
 #'  GOLevel =GOLev, listNames=names4lists)
 #'@export
-crossTabGOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
+crossTabGOIDsUnrestricted <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
 {
   levelIDs <- goProfiles::getGOLevel('BP', level=GOLevel)
   levelInGO1 <- levelIDs %in% GO1
@@ -72,10 +73,43 @@ crossTabGOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
   crossedTable <- table(levelInGO1, levelInGO2, dnn=dnnNames)
 }
 
-
-#' StandardTest4GOIDs
+#' GOIDsInLevel
 #'
-#' This function performs a "standard test" of equivalence between two lists of enriched GOTerms
+#' This function extends getGOLevel returning only GO identifiers appearing between GO ancestors of at least one GeneList
+#'
+#' @param GOLev An integer
+#' @param onto string describing the ontology. Belongs to c('BP', 'MF', 'CC', 'ANY')
+#' @param geneList1 character vector containing a FIRST gene list of entrez IDs
+#' @param geneList2 character vector containing a SECOND gene list of entrez IDs
+#' @param orgPackage A string wih the name of the annotation package
+#' @restricted Boolean variable to decide how tabulation is performed. 
+#' @examples
+#'  data(kidneyGeneLists)
+#'  GOLev <- 3
+#'  anOnto <- 'BP'
+#'  gl1 <- kidneyGeneLists[[1]]
+#'  gl2 <- kidneyGeneLists[[2]]
+#'  orgPkg ='org.Hs.eg.db'
+#'  GOIDSinLevel.1 <- GOIDsInLevel (GOLev=GOLev, onto = anOnto,  restricted=FALSE); 
+#'  length(GOIDSinLevel.1)
+#'  GOIDSinLevel.2 <- GOIDsInLevel (GOLev=GOLev, onto = anOnto,  geneList1 = gl1, geneList2 = gl2, orgPackage =orgPkg)
+#'  length(GOIDSinLevel.2)
+#'@export
+GOIDsInLevel <- function (GOLev, onto, geneList1=NULL, geneList2 =NULL, orgPackage=NULL, restricted=TRUE){
+  levelIDs <- goProfiles::getGOLevel(onto=onto, level=GOLev)
+  if (restricted && (!is.null(geneList1)&&(!is.null(geneList2))&&(!is.null(orgPackage)))){
+    specGOIDs1 <- goProfiles::GOTermsList(geneList1, onto=onto, orgPkg = orgPackage)
+    specGOIDs2 <- goProfiles::GOTermsList(geneList2, onto=onto, orgPkg = orgPackage)
+    allGOIDs1 <-unique(unlist(goProfiles::getAncestorsLst(specGOIDs1, onto=onto)))
+    allGOIDs2 <-unique(unlist(goProfiles::getAncestorsLst(specGOIDs2, onto=onto)))
+    levelIDs <- intersect(levelIDs, union(allGOIDs1, allGOIDs2))
+  }
+  return(levelIDs)
+}
+
+#' crossTabGOIDs
+#'
+#' This function performs a crosstabulation between two lists of enriched GOTerms
 #' The lists are intended to have been obtained from enrichment analyses performed on two gene lists
 #'
 #' @param GO1 character vector containing a FIRST list of GO identifiers
@@ -83,10 +117,64 @@ crossTabGOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
 #' @param onto string describing the ontology. Belongs to c('BP', 'MF', 'CC', 'ANY')
 #' @param GOLev An integer
 #' @param listNames character vector with names of the genelists that generated the enriched GOIDs
+#' @param geneList1 character vector containing a FIRST gene list of entrez IDs
+#' @param geneList2 character vector containing a SECOND gene list of entrez IDs
+#' @param orgPackage A string wih the name of the annotation package
+#' @restricted Boolean variable to decide how tabulation is performed. 
+#' Unrestricted tabulation crosses _all_ GO Terms located at the level indicated by `GOLev` with the two GOIDs lists 
+#' Restricted tabulation crosses only terms from the selected GO level that are _common to ancestor terms of either list_. 
+#' That is, if one term in the selected GO level is not an ancestor of at least one of the gene list most specific GO terms 
+#' it is excluded from the GO Level's terms because it is impossible that it appears as being enriched.
 #' @examples
 #'  data(kidneyEnrichedGOIDs)
 #'  GOIDs1 <-kidneyEnrichedGOIDs[[1]]
 #'  GOIDs2 <-kidneyEnrichedGOIDs[[2]]
+#'  names4lists<- names(kidneyEnrichedGOIDs)[1:2]
+#'  anOnto <- 'BP'
+#'  GOLev <- 3
+#'  gl1 <- kidneyGeneLists[[1]]
+#'  gl2 <- kidneyGeneLists[[2]]
+#'  orgPkg ='org.Hs.eg.db'
+#'  restrictTab <- TRUE
+#'  crossTabbedGOIdsRestricted <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, 
+#'  GOLevel =GOLev, listNames=names4lists, geneList1 = gl1, geneList2 = gl2, orgPackage =orgPkg, restricted = restrictTab)
+#'  show(crossTabbedGOIdsRestricted)
+#'  restrictTab <- FALSE
+#'  crossTabbedGOIdsUnrestricted <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, 
+#'  GOLevel =GOLev, listNames=names4lists, geneList1 = gl1, geneList2 = gl2, orgPackage =orgPkg, restricted = restrictTab)
+#'  show(crossTabbedGOIdsUnrestricted)
+#'@export
+crossTabGOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL, 
+                           geneList1=NULL, geneList2=NULL, orgPackage = NULL, restricted=FALSE)
+{
+  levelIDs<- GOIDsInLevel (GOLev=GOLevel, onto = onto,  geneList1 = geneList1, geneList2 = geneList2, 
+                           orgPackage =orgPackage, restricted=restricted)
+  levelInGO1 <- levelIDs %in% GO1
+  levelInGO2 <- levelIDs %in% GO2
+  if (is.null(listNames)) {
+    dnnNames<-paste0("Enriched_in_", c("list_1", "list_2"))
+  }else{
+    dnnNames<-paste0("Enriched_in_", listNames)
+  }
+  crossedTable <- table(levelInGO1, levelInGO2, dnn=dnnNames)
+}
+
+#' StandardTest4GOIDs
+#'
+#' This function performs a "standard test" of equivalence between two lists of enriched GOTerms
+#' The lists are intended to have been obtained from enrichment analyses performed on two gene lists
+#' Given that this version of the test does not provide gene lists only "unrestricted" crosstabulation can be performed
+#' That is, all terms in the given GO level are used to build the table, even if they are not found in the geneLists's annotations.
+#'
+#' @param GO1 character vector containing a FIRST list of GO identifiers
+#' @param GO2 character vector containing a SECOND gene list GO identifiers
+#' @param onto string describing the ontology. Belongs to c('BP', 'MF', 'CC', 'ANY')
+#' @param GOLev An integer
+#' @param listNames character vector with names of the genelists that generated the enriched GOIDs
+#' @examples
+#'  GOIDs2 <-kidneyEnrichedGOIDs[[2]]
+#'  data(kidneyEnrichedGOIDs)
+#'  GOIDs1 <-kidneyEnrichedGOIDs[[1]]
 #'  names4lists<- names(kidneyEnrichedGOIDs)[1:2]
 #'  anOnto <- 'BP'
 #'  GOLev<- 3
@@ -96,7 +184,7 @@ crossTabGOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
 stdTest4GOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
 {
   crossTabbedGOIds <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, 
-                                     GOLevel =GOLev, listNames=names4lists)
+                                     GOLevel =GOLev, listNames=names4lists, restricted=FALSE)
   fisher.test (crossTabbedGOIds, alt="g")
 }
 
@@ -110,6 +198,11 @@ stdTest4GOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
 #' @param orgPackage A string wih the name of the annotation package
 #' @param onto string describing the ontology. Belongs to c('BP', 'MF', 'CC', 'ANY')
 #' @param GOLev An integer
+#' @restricted Boolean variable to decide how tabulation of GOIDs is performed. 
+#' Unrestricted tabulation crosses _all_ GO Terms located at the level indicated by `GOLev` with the two GOIDs lists 
+#' Restricted tabulation crosses only terms from the selected GO level that are _common to ancestor terms of either list_. 
+#' That is, if one term in the selected GO level is not an ancestor of at least one of the gene list most specific GO terms 
+#' it is excluded from the GO Level's terms because it is impossible that it appears as being enriched.
 #' @param pAdjustMeth string describing the adjust method. Belongs to c('BH', 'BY', 'Bonf')
 #' @param pvalCutoff A numeric value
 #' @param qvalCutoff A numeric value
@@ -120,21 +213,27 @@ stdTest4GOIDs <- function (GO1, GO2, onto, GOLevel, listNames=NULL)
 #'  gl2 <-kidneyGeneLists[[2]]
 #'  anOnto <- 'BP'
 #'  GOLev<- 3
+#'  restricted <- FALSE
 #'  adjMeth<- 'BH'
 #'  pValCut <- 0.05
 #'  qValCut <- 0.01
-#'  crossTabFromGeneLists <- crossTab4GeneLists (genelist1=gl1, genelist2=gl2,
-#'  geneUniverse=humanEntrezIDs, orgPackg="org.Hs.eg.db", onto=anOnto, GOLevel=GOLev)
-#'
+#'  crossTabFromGeneListsUnrestricted <- crossTabGOIDs4GeneLists (genelist1=gl1, genelist2=gl2,
+#'  geneUniverse=humanEntrezIDs, orgPackg="org.Hs.eg.db", onto=anOnto, GOLevel=GOLev, restricted=restricted)
+#'  restricted <- TRUE
+#'  crossTabFromGeneListsRestricted <- crossTabGOIDs4GeneLists (genelist1=gl1, genelist2=gl2,
+#'  geneUniverse=humanEntrezIDs, orgPackg="org.Hs.eg.db", onto=anOnto, GOLevel=GOLev, restricted=restricted)
+
 #'@export 
-crossTabGOIDs4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPackg, onto, GOLevel,
+crossTabGOIDs4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPackg, onto, GOLevel, restricted =FALSE,
                                pAdjustMeth="BH", pvalCutoff=0.01, qvalCutoff=0.05)
 {
   enriched1 <- clusterProfiler::enrichGO(gene=genelist1, universe=geneUniverse, OrgDb=orgPackg, ont=anOnto, pvalueCutoff=pvalCutoff)
   GOIDs1 <- as.character(as.data.frame(enriched1)$ID)
   enriched2 <- clusterProfiler::enrichGO(gene=genelist2, universe=geneUniverse, OrgDb=orgPackg, ont=anOnto, pvalueCutoff=pvalCutoff)
   GOIDs2 <- as.character(as.data.frame(enriched2)$ID)
-  crossTabGOIDs4GeneLists <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, GOLevel =GOLevel)
+  crossTabGOIDs4GeneLists <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, GOLevel =GOLevel,
+                                            geneList1=genelist1, geneList2=genelist2, orgPackage = orgPackg, 
+                                            restricted = restricted)
 }
 
 
@@ -149,6 +248,11 @@ crossTabGOIDs4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPack
 #' @param orgPackage A string wih the name of the annotation package
 #' @param onto string describing the ontology. Belongs to c('BP', 'MF', 'CC', 'ANY')
 #' @param GOLev An integer
+#' @restricted Boolean variable to decide how tabulation is performed. 
+#' Unrestricted tabulation crosses _all_ GO Terms located at the level indicated by `GOLev` with the two GOIDs lists 
+#' Restricted tabulation crosses only terms from the selected GO level that are _common to ancestor terms of either list_. 
+#' That is, if one term in the selected GO level is not an ancestor of at least one of the gene list most specific GO terms 
+#' it is excluded from the GO Level's terms because it is impossible that it appears as being enriched.
 #' @param pAdjustMeth string describing the adjust method. Belongs to c('BH', 'BY', 'Bonf')
 #' @param pvalCutoff A numeric value
 #' @param qvalCutoff A numeric value
@@ -159,6 +263,7 @@ crossTabGOIDs4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPack
 #'  gl2 <-kidneyGeneLists[[2]]
 #'  anOnto <- 'BP'
 #'  GOLev<- 3
+#'  restricted = FALSE
 #'  adjMeth<- 'BH'
 #'  pValCut <- 0.05
 #'  qValCut <- 0.01
@@ -166,13 +271,20 @@ crossTabGOIDs4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPack
 #'  geneUniverse=humanEntrezIDs, orgPackg="org.Hs.eg.db", onto=anOnto, GOLevel=GOLev)
 #'
 #'@export 
-stdTest4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPackg, onto, GOLevel,
+stdTest4GeneLists <- function (genelist1, genelist2, geneUniverse, orgPackg, onto, GOLevel, restricted=FALSE,
                                pAdjustMeth="BH", pvalCutoff=0.01, qvalCutoff=0.05)
 {
-  enriched1 <- enrichOnto(gene=genelist1, universe=geneUniverse, OrgDb=orgPackg, ont=anOnto, pvalueCutoff=pvalCutoff)
+  enriched1 <- enrichOnto(geneList = genelist1, geneUniverse = geneUniverse, 
+                          orgPackage = orgPackg, onto = onto, 
+                          pvalCutoff=pvalCutoff, qvalCutoff=qvalCutoff, pAdjustMeth=pAdjustMeth)
   GOIDs1 <- as.character(as.data.frame(enriched1)$ID)
-  enriched2 <- enrichOnto(gene=genelist2, universe=geneUniverse, OrgDb=orgPackg, ont=anOnto, pvalueCutoff=pvalCutoff)
+  enriched2 <- enrichOnto(geneList = genelist2, geneUniverse=geneUniverse, 
+                          orgPackage = orgPackg, onto = onto, 
+                          pvalCutoff=pvalCutoff, qvalCutoff=qvalCutoff, pAdjustMeth=pAdjustMeth)
   GOIDs2 <- as.character(as.data.frame(enriched2)$ID)
-  stdTest<- stdTest4GOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, GOLevel =GOLevel)
+  crossTabbedGOIDs4GeneLists <- crossTabGOIDs (GO1 = GOIDs1, GO2 = GOIDs2, onto = anOnto, GOLevel =GOLevel,
+                                            geneList1=genelist1, geneList2=genelist2, orgPackage = orgPackg, 
+                                            restricted = restricted)
+  fisher.test (crossTabbedGOIDs4GeneLists, alt="g")
 }
 
